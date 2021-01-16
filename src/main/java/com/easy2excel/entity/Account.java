2012b560b172d6aaa9826318cf7d3a3ea3c7d8d5
anil <anilkumarsahoo17@gmail.com>
2021-01-16 15:34:33 +0530

    added entity class

--- src/main/java/com/easy2excel/entity/Account.java
+++ src/main/java/com/easy2excel/entity/Account.java
@@ -6,13 +6,19 @@ import lombok.ToString;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "account_details", uniqueConstraints = {@UniqueConstraint(name = "account_ident_unq", columnNames = {"account_id"})})
+@Table(name = "account_details", uniqueConstraints = {
+        @UniqueConstraint(
+                name = "account_ident_unq",
+                columnNames = {"account_id"}
+        )})
 @Getter
 @Setter
 @ToString
-public class AccountDetails extends Auditable{
+public class Account extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "AccountIdSeq")
     @SequenceGenerator(name = "AccountIdSeq", sequenceName = "account_id_seq", allocationSize = 10)
@@ -20,10 +26,13 @@ public class AccountDetails extends Auditable{
     private Long id;
 
     @NotBlank
-    @Column(name = "name",length = 60,nullable = false)
+    @Column(name = "name", length = 60, nullable = false)
     private String name;
 
     @Column(name = "url", length = 60)
     private String url;
+
+    @OneToMany(mappedBy = "account",cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.LAZY)
+    private List<Opportunity> opportunities = new ArrayList<>();
 
 }
