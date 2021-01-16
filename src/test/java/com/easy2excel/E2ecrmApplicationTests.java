1bf082269112b18800af7d66118abf4a84ed7894
anil <anilkumarsahoo17@gmail.com>
2021-01-16 10:42:47 +0530

    initial commit for the application setup

---
+++ src/test/java/com/easy2excel/E2ecrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy2excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2ecrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
